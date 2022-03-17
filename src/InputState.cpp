aa0afbbb217c66c556db9ccb1df7afacf5ed5712
Lukhnos Liu <lukhnos@lukhnos.org>
2022-03-16 18:48:56 -0700

    Tighten the immutability contract in InputState

--- src/InputState.cpp
+++ src/InputState.cpp
@@ -1,40 +1,69 @@
+// Copyright (c) 2022 and onwards The McBopomofo Authors.
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 #include "InputState.h"
 
 namespace McBopomofo {
 
-std::string InputStateEmpty::composingBuffer() { return ""; }
+std::string InputStateEmpty::composingBuffer() const { return ""; }
 
-std::string InputStateEmptyIgnoringPrevious::composingBuffer() { return ""; }
+std::string InputStateEmptyIgnoringPrevious::composingBuffer() const {
+  return "";
+}
 
-void InputStateCommitting::setPoppedText(std::string &poppedText) {
+void InputStateCommitting::setPoppedText(const std::string& poppedText) {
   m_poppedText = poppedText;
 }
 
-std::string InputStateCommitting::poppedText() { return m_poppedText; }
+std::string InputStateCommitting::poppedText() const { return m_poppedText; }
 
-void InputStateNotEmpty::setComposingBuffer(std::string &composingBuffer) {
+void InputStateNotEmpty::setComposingBuffer(
+    const std::string& composingBuffer) {
   m_composingBuffer = composingBuffer;
 }
 
-std::string InputStateNotEmpty::composingBuffer() { return m_composingBuffer; }
+std::string InputStateNotEmpty::composingBuffer() const {
+  return m_composingBuffer;
+}
 
 void InputStateNotEmpty::setCursorIndex(size_t index) { m_cursorIndex = index; }
 
-size_t InputStateNotEmpty::cursorIndex() { return m_cursorIndex; }
+size_t InputStateNotEmpty::cursorIndex() const { return m_cursorIndex; }
 
-void InputStateInputting::setPoppedText(std::string &poppedText) {
+void InputStateInputting::setPoppedText(const std::string& poppedText) {
   m_poppedText = poppedText;
 }
 
-std::string InputStateInputting::poppedText() { return m_poppedText; }
+std::string InputStateInputting::poppedText() const { return m_poppedText; }
 
 void InputStateChoosingCandidate::setCandidates(
-    std::vector<std::string> &candidates) {
+    const std::vector<std::string>& candidates) {
   m_candidates = candidates;
 }
 
-std::vector<std::string> InputStateChoosingCandidate::candidates() {
+const std::vector<std::string>& InputStateChoosingCandidate::candidates()
+    const {
   return m_candidates;
 }
 
-} // namespace McBopomofo
+}  // namespace McBopomofo
--- src/InputState.h
+++ src/InputState.h
@@ -1,5 +1,28 @@
-#ifndef _FCITX5_MCBOPOMOFO_INPUTSTATE_H_
-#define _FCITX5_MCBOPOMOFO_INPUTSTATE_H_
+// Copyright (c) 2022 and onwards The McBopomofo Authors.
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef SRC_INPUTSTATE_H_
+#define SRC_INPUTSTATE_H_
 
 #include <string>
 #include <vector>
@@ -7,60 +30,60 @@
 namespace McBopomofo {
 
 class InputState {
-public:
-    virtual ~InputState() = default;
+ public:
+  virtual ~InputState() = default;
 };
 
 class InputStateEmpty : public InputState {
-public:
-  std::string composingBuffer();
+ public:
+  std::string composingBuffer() const;
 };
 
 class InputStateEmptyIgnoringPrevious : public InputState {
-public:
-  std::string composingBuffer();
+ public:
+  std::string composingBuffer() const;
 };
 
 class InputStateCommitting : public InputState {
-public:
-  void setPoppedText(std::string &poppedText);
-  std::string poppedText();
+ public:
+  void setPoppedText(const std::string& poppedText);
+  std::string poppedText() const;
 
-protected:
+ protected:
   std::string m_poppedText;
 };
 
 class InputStateNotEmpty : public InputState {
-public:
-  void setComposingBuffer(std::string &composingBuffer);
-  std::string composingBuffer();
+ public:
+  void setComposingBuffer(const std::string& composingBuffer);
+  std::string composingBuffer() const;
 
   void setCursorIndex(size_t cursorIndex);
-  size_t cursorIndex();
+  size_t cursorIndex() const;
 
-protected:
+ protected:
   std::string m_composingBuffer;
   size_t m_cursorIndex;
 };
 
 class InputStateInputting : public InputStateNotEmpty {
-public:
-  void setPoppedText(std::string &poppedText);
-  std::string poppedText();
+ public:
+  void setPoppedText(const std::string& poppedText);
+  std::string poppedText() const;
 
-protected:
+ protected:
   std::string m_poppedText;
 };
 
 class InputStateChoosingCandidate : public InputStateNotEmpty {
-public:
-  void setCandidates(std::vector<std::string> &candidates);
-  std::vector<std::string> candidates();
+ public:
+  void setCandidates(const std::vector<std::string>& candidates);
+  const std::vector<std::string>& candidates() const;
 
-protected:
+ protected:
   std::vector<std::string> m_candidates;
 };
 
-} // namespace McBopomofo
+}  // namespace McBopomofo
 
-#endif
+#endif  // SRC_INPUTSTATE_H_
