4614354aa759764be3415415475df5abe2b6e92d
Weng Xuetian <wengxt@gmail.com>
2022-03-29 10:19:33 -0700

    Extract and merge translation to .conf files used by fcitx

--- run-xgettext.sh
+++ run-xgettext.sh
@@ -1,15 +1,28 @@
 xgettext \
-	--package-name=fcitx-mcbopomofo \
+	--package-name=fcitx5-mcbopomofo \
 	--package-version=v0.1 \
 	--copyright-holder=OpenVanilla \
 	--c++ \
 	-k_ \
 	-kN_ \
-	-o po/fcitx-mcbopomofo.pot \
+	-o po/fcitx5-mcbopomofo.pot \
     src/McBopomofo.cpp src/McBopomofo.h src/KeyHandler.cpp src/KeyHandler.h
 
-## To re-generate the po files from scratch:
-# msginit -l zh_TW.UTF-8 --no-translator -o po/zh_TW.po -i po/po/fcitx-mcbopomofo.pot
+xgettext \
+	--package-name=fcitx5-mcbopomofo \
+	--language=Desktop \
+	-k \
+	--keyword=Name \
+	--keyword=GenericName \
+	--keyword=Comment \
+	--keyword=Keywords \
+	-j \
+	-o po/fcitx5-mcbopomofo.pot \
+	src/mcbopomofo.conf.in.in src/mcbopomofo-addon.conf.in.in
 
-msgmerge --update po/en.po po/fcitx-mcbopomofo.pot
-msgmerge --update po/zh_TW.po po/fcitx-mcbopomofo.pot
+
+## To re-generate the po files from scratch:
+# msginit -l zh_TW.UTF-8 --no-translator -o po/zh_TW.po -i po/po/fcitx5-mcbopomofo.pot
+
+msgmerge --update po/en.po po/fcitx5-mcbopomofo.pot
+msgmerge --update po/zh_TW.po po/fcitx5-mcbopomofo.pot
