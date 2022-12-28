60c13e6a7be7ad399ce94a9a0a961131d22a12f3
BrLi <brli@chakralinux.org>
2022-12-28 21:44:41 +0800

    update strings for translation
    - do not ignore .pot file
    - edit string generation script to include new .xml file
    - update .po files with new script

--- run-xgettext.sh
+++ run-xgettext.sh
@@ -1,6 +1,6 @@
 xgettext \
 	--package-name=fcitx5-mcbopomofo \
-	--package-version=v0.1 \
+	--package-version=2.4 \
 	--copyright-holder=OpenVanilla \
 	--c++ \
 	--from-code=UTF-8 \
@@ -21,8 +21,15 @@ xgettext \
 	-o po/fcitx5-mcbopomofo.pot \
 	src/mcbopomofo.conf.in.in src/mcbopomofo-plain.conf.in.in src/mcbopomofo-addon.conf.in.in
 
+xgettext \
+	--package-name=fcitx5-mcbopomofo \
+	--copyright-holder=OpenVanilla \
+	-j \
+	-o po/fcitx5-mcbopomofo.pot \
+	org.fcitx.Fcitx5.Addon.McBopomofo.metainfo.xml.in
+
 ## To re-generate the po files from scratch:
-# msginit -l zh_TW.UTF-8 --no-translator -o po/zh_TW.po -i po/po/fcitx5-mcbopomofo.pot
+# msginit -l zh_TW.UTF-8 --no-translator -o po/zh_TW.po -i po/fcitx5-mcbopomofo.pot
 
 msgmerge --update po/en.po po/fcitx5-mcbopomofo.pot
 msgmerge --update po/zh_TW.po po/fcitx5-mcbopomofo.pot
