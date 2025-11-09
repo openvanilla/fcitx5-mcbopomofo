fcf7bd0cbd8e30399af525266e98d96a6abf4d19
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2025-11-09 19:30:05 +0800

    Refactor isValidSingleUtf8Character to use ICU U8_NEXT macro

--- src/Big5Utils/Big5Utils.cpp
+++ src/Big5Utils/Big5Utils.cpp
@@ -33,41 +33,14 @@ bool isValidSingleUtf8Character(const char* str, int32_t length) {
   if (str == nullptr || length <= 0) {
     return false;
   }
-
-  int32_t charCount = 0;
+  const uint8_t* s = reinterpret_cast<const uint8_t*>(str);
   int32_t i = 0;
-
-  while (i < length) {
-    unsigned char byte = static_cast<unsigned char>(str[i]);
-    int32_t charLen = 0;
-
-    if ((byte & 0x80) == 0x00) {
-      charLen = 1;
-    } else if ((byte & 0xE0) == 0xC0) {
-      charLen = 2;
-    } else if ((byte & 0xF0) == 0xE0) {
-      charLen = 3;
-    } else if ((byte & 0xF8) == 0xF0) {
-      charLen = 4;
-    } else {
-      return false;
-    }
-
-    if (i + charLen > length) {
-      return false;
-    }
-
-    for (int32_t j = 1; j < charLen; j++) {
-      if ((static_cast<unsigned char>(str[i + j]) & 0xC0) != 0x80) {
-        return false;
-      }
-    }
-
-    i += charLen;
-    charCount++;
+  UChar32 c;
+  U8_NEXT(s, i, length, c);
+  if (c < 0) {  // Invalid sequence
+    return false;
   }
-
-  return charCount == 1;
+  return i == length;  // Check if we consumed the whole string
 }
 
 std::string convertBig5fromUint16(uint16_t codePoint) {
