af80863abfa9a1c6d297fda90a2054c75b018fd7
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2025-11-28 11:55:50 +0800

    refactor: use modern CMake commands for building and installing the project

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# rm -rf build
-mkdir -p build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug # use Debug for easy debugging with gdb
-make                                                          # or ninja, depending on your system
-sudo make install                                             # or sudo ninja install
+cmake -B build -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug # use Debug for easy debugging with gdb
+cmake --build build                                                 # or ninja, depending on your system
+sudo cmake --install build                                          # or sudo ninja install
